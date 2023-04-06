97fa26f5397c3b301f47a2685df74b589b6be586
Corey Mutnik <cmutnik@hawaii.edu>
2023-04-06 07:02:51 -0400

    df page demo

--- pages/3_⚙️_Remove_Image_Background.py
+++ pages/3_⚙️_Remove_Image_Background.py
@@ -13,7 +13,7 @@ st.write(
     ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](https://github.com/tyler-simons/BackgroundRemoval) on GitHub. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
 )
 st.sidebar.write("## Upload and download :gear:")
-
+st.markdown("Original code found [here.](https://github.com/tyler-simons/BackgroundRemoval)")
 
 # Download the fixed image
 def convert_image(img):
